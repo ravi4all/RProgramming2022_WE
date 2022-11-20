b3820cffdc2316c14d0030b25223727636ef9282
Ravikant Tyagi <dhruvasharma1989@gmail.com>
2022-11-20 10:54:20 +0530

    Add files via upload

--- data_visualization/data_visualization.R
+++ data_visualization/data_visualization.R
@@ -29,6 +29,82 @@ ggplot(df, aes(x = price)) +
   geom_histogram(col='red', fill='green', bins = 70)
 
 
+# barplot
+ggplot(df, aes(x = fuel)) + geom_bar()
+ggplot(df, aes(x = rooms)) + geom_bar()
+ggplot(df, aes(x = rooms)) + geom_bar(position = "fill")
+
+ggplot(df, aes(x=waterfront, fill = centralAir)) + 
+  geom_bar(position = "fill")
+
+ggplot(df, aes(x=waterfront, fill = sewer)) + 
+  geom_bar(position = "fill")
+
+ggplot(df, aes(x=rooms, fill = waterfront)) + 
+  geom_bar(position = "fill")
+
+
+
+# scatterplot
+
+ggplot(df, aes(x = price, y = livingArea)) + geom_point()
+ggplot(df, aes(x = price, y = landValue)) + geom_point()
+
+ggplot(df, aes(x = price, y = livingArea)) + 
+  geom_point() + geom_smooth()
+
+# lm - linear model
+ggplot(df, aes(x = price, y = livingArea)) + 
+  geom_point() + geom_smooth(method = "lm")
+
+
+# boxplot
+ggplot(df, aes(x = price)) + geom_boxplot()
+
+ggplot(df, aes(x=waterfront, y=price)) + geom_boxplot()
+
+ggplot(df, aes(x=factor(rooms), y=price)) + 
+  geom_boxplot()
+
+
+
+# facet grid - subplots
+ggplot(df, aes(x=price, y=livingArea, col = centralAir)) + 
+  geom_point() + geom_smooth(method = "lm")
+
+ggplot(df, aes(x=price, y=livingArea, col = centralAir)) + 
+  geom_point() + geom_smooth(method = "lm") +
+  facet_grid(~centralAir)
+
+ggplot(df, aes(x=price, y=livingArea, col = sewer)) + 
+  geom_point() + geom_smooth(method = "lm") +
+  facet_grid(~sewer)
+
+
+ggplot(df, aes(x=price, y=livingArea, col=heating)) + 
+  geom_point() + geom_smooth(method = "lm") +
+  facet_grid(waterfront ~ heating)
+
+
+
+# themes and styling
+
+obj_1 <- ggplot(df, aes(x=factor(rooms), y=price)) + 
+  geom_boxplot()
+
+obj_2 <- obj_1 + labs(title = 'Price w.r.t rooms',
+                      subtitle = "Rooms are available from 2 to 12",
+                      x = "Number of Rooms",
+                      y = "Price")
+
+obj_2 + theme_dark()
+obj_2 + theme_classic()
+obj_2 + theme_gray()
+
+obj_2 + theme(panel.background = element_rect(fill = "palegreen"))
+
+
+
 
 
 
---
+++ data_visualization/olympics_exercise.R
@@ -0,0 +1,9 @@
+# Exercise on Olympics
+# 1. Find male athlete with most gold medals
+# 2. Find female athlete with most gold medals
+# 3. Find country with most medals, sort in descending order
+# 4. Print Medal tally, country name - gold, silver, bronze
+# 5. Analyze olympics data for India
+#     - Total number of gold, silver, bronze
+#     - In which sports India has won most medals
+# 6. Visualize data on graph using ggplot
